d171ed789c71a92a419ba91514a7f0680bf09d8e
Punith Nandiraj <punithnandiraj@gmail.com>
2023-02-24 21:39:28 +0530

    Update property-details.tsx

--- client/src/pages/property-details.tsx
+++ client/src/pages/property-details.tsx
@@ -40,7 +40,7 @@ const PropertyDetails = () => {
     const isCurrentUser = user.email === propertyDetails.creator.email;
 
     const handleDeleteProperty = () => {
-        const response = confirm(
+        const response = window.confirm(
             "Are you sure you want to delete this property?",
         );
         //eslint-disable-line
